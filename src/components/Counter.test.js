a400c66b0d07e6e05db8f1275b3cf3fc95a2f5eb
unknown <dbdf0369@gmail.com>
2020-10-23 21:01:20 +0800

    add express start server

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,10 +1,13 @@
-import { render } from "@testing-library/vue";
+import { fireEvent, render } from "@testing-library/vue";
 import Counter from "./Counter.vue";
 
-test('Original title is 「當前點擊次數0」', () => {
-  const { getByTestId } = render(Counter);
+test('Original title is 「當前點擊次數0」', async () => {
+  const { getByTestId, getByText } = render(Counter);
+
+  const incrementButton = getByText('點我 + 1');
+  await fireEvent.click(incrementButton);
 
   const title = getByTestId('currentCounter');
 
-  expect(title.innerHTML).toBe('當前點擊次數0');
+  expect(title.innerHTML).toBe('當前點擊次數1');
 })
