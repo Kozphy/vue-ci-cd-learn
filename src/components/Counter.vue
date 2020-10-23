69c3ec491f40b6873067d01f4beb6cc3f089e632
unknown <dbdf0369@gmail.com>
2020-10-23 19:04:45 +0800

    add test

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>當前點擊次數{{ count }}</h1>
+    <h1 data-testid="currentCounter">當前點擊次數{{ count }}</h1>
     <button
       type="button"
       @click="increment()"
